e154e88da2188d3dd4a0b8b8065aa89d1c9ef680
morgacoo <morgacoo@gmail.com>
2017-08-17 10:20:22 -0500

    Deaf grandma random generator and program stop

--- calc.rb
+++ calc.rb
@@ -1,15 +1,56 @@
-comment = gets.chomp
+say1 = ''
+say2 = ''
+say3 = ''
 
-while comment != 'BYE'
-  if comment != comment.upcase
-    puts 'HUH?! SPEAK UP, HONEY'
-    comment = gets.chomp
-  elsif comment == comment.upcase
-    year = (rand(1900..2000))
+while say1 != 'BYE' || say2 != 'BYE' || say3 != 'BYE'
+
+while say1 != 'BYE'
+
+  say1 = gets.chomp
+
+if say1 == say1.upcase
+  year = rand(1929..2000)
+  puts 'NO, NOT SINCE ' + year.to_s + '!'
+else
+  puts 'HUH?! SPEAK UP, SONNY!'
+end
+
+if say1 == 'BYE'
+  say2 = ''
+  say3 = ''
+end
+end
+
+while say2 != 'BYE'
+  say2 = gets.chomp
+
+  if say2 == say2.upcase
+    year = (rand(1929..2000))
     puts 'NO, NOT SINCE ' + year.to_s + '!'
-    comment = gets.chomp
   else
-    puts 'BYE!'
+    puts 'HUH?! SPEAK UP, SONNY!'
   end
+
+  if say2 !='BYE'
+    say1 = ''
+    say3 = ''
+  end
+end
+
+while say3 != 'BYE'
+  say3 = gets.chomp
+
+  if say3 == say3.upcase
+    year = rand(1929..2000)
+    puts 'NO, NOT SINCE ' + year.to_s + '!'
+  else
+    puts 'HUH?! SPEAK UP, SONNY!'
+  end
+
+  if say3 != 'BYE'
+    say1 = ''
+    say2 = ''
+  end
+end
 
 end
