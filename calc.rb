58c5733aa3ffb863df7d18d4754e77f3b59666f8
morgacoo <morgacoo@gmail.com>
2017-08-18 11:26:25 -0500

    program to give you spanks for every year alive

--- calc.rb
+++ calc.rb
@@ -1,12 +1,53 @@
-birthday = Time.mktime(1990, 8, 17, 9, 42)
-current_date = Time.new
-age_seconds = current_date - birthday
+#****Program to give you a "Spank" for every year you are old!
 
-billions = 1000000000 - age_seconds.to_i #how many seconds left until you hit 1 billion
+puts 'What year were you born?'
+year = gets.chomp
+puts 'What month were you born?'
+month = gets.chomp
+puts 'What day were you born?'
+day = gets.chomp
 
-time_left_years = (billions.to_f/60/60/24/365)
-time_left_hours = (billions.to_f/60/60/24)
+if month.downcase == 'january'
+  month = 1
+  puts 'Jan!'
+elsif month.downcase == 'february'
+  month = 2
+elsif month.downcase == 'march'
+  month = 3
+elsif month.downcase == 'april'
+  month = 4
+elsif month.downcase == 'may'
+  month = 5
+elsif month.downcase == 'june'
+  month = 6
+elsif month.downcase == 'july'
+  month = 7
+elsif month.downcase == 'august'
+  month = 8
+elsif month.downcase == 'september'
+  month = 9
+elsif month.downcase == 'october'
+  month = 10
+elsif month.downcase == 'november'
+  month = 11
+elsif month.downcase == 'december'
+end
 
-puts 'You are ' + age_seconds.to_i.to_s + ' seconds old!'
-puts 'You will be 1 billion seconds old in ' + time_left_years.to_s + ' years!'
-puts 'Or if you prefer days, you will be 1 billion seconds old in ' + time_left_hours.to_s + ' days!'
+birthday = Time.mktime(year, month, day)
+
+current_time = Time.new
+age_seconds = current_time.to_i - birthday.to_i
+
+age_years = age_seconds.to_i/60/60/24/365
+
+puts
+puts 'You\'re ' + age_years.to_s + ' eh...'
+puts
+i = 0
+while i <= age_years
+  puts "SPANK!"
+  i += 1
+end
+
+puts
+puts 'Wait til next year!'
