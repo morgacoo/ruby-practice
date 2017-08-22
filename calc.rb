37e0751980df68f10c71ee626cb609882e8f9000
morgacoo <morgacoo@gmail.com>
2017-08-22 08:51:00 -0500

    code for growing and orange tree

--- calc.rb
+++ calc.rb
@@ -1,115 +1,86 @@
-class Dragon
+class Orange_tree
 
-  def initialize name
-    @name = name
-    @asleep = false
-    @stuff_in_belly     = 10  # He's full
-    @stuff_in_intestine =  0  # He doesn't need to go to bathroom
-
-    puts @name + ' is born.'
+  def initialize
+    @height = 3  #Height 3 feet tall when planted
+    @orange_count = 0 #No oranges when planted
+    @grows_fruit = false
+    @years_old = 0
+    puts "You planted a baby orange tree! It's 3 feet tall!"
   end
 
-  def feed
-    puts 'You feed ' + @name + '.'
-    @stuff_in_belly = 10
-    passage_of_time
+  def height
+    puts 'Your tree is ' + @height.to_s + ' feet tall!'
+    one_year_passes
   end
 
-  def walk
-    puts 'You walk ' + @name + '.'
-    @stuff_in_intestine = 0
-    passage_of_time
-  end
+  def one_year_passes
+    if (@years_old >= 0 && @years_old<= 3)
+      @height = @height + 3 #add 3 feet in height each year
+      @years_old = @years_old + 1
+      puts 'Your tree grew 4 feet this year, but still doesn\'t have any oranges. :-('
+      puts 'Your tree is ' + @height.to_s + ' feet tall!'
+    elsif (@years_old >=4 && @years_old <= 6)
+      @height = @height + 2 #adds 2 feet to height each year
+      @years_old = @years_old + 1
+      @grows_fruit = true
+      @orange_count = @orange_count + 2 #grows two a year
+      puts 'Your tree grew 3 feet this year and grew 2 oranges! You now have ' + @orange_count.to_s + ' oranges.'
+      puts 'Your tree is ' + @height.to_s + ' feet tall!'
 
-  def put_to_bed
-    puts 'You put ' + @name + ' to bed.'
-    @asleep = true
-    3.times do
-      if @asleep
-        passage_of_time
-      end
-      if @asleep
-        puts @name + ' snores, filling the room with smoke.'
-      end
-    end
-    if @asleep
-      @asleep = false
-      puts @name + ' wakes up slowly.'
-    end
-  end
-
-  def toss
-    puts 'You toss ' + @name + ' up into the air.'
-    puts 'He giggles, which singes your eyebrows.'
-    passage_of_time
-  end
-
-  def rock
-    puts 'You rock ' + @name + ' gently.'
-    @asleep = true
-    puts 'He briefly dozes off...'
-    passage_of_time
-    if @asleep
-      @asleep = false
-      puts '...but wakes when you stop.'
-    end
-  end
-
-  private  #methods are defined by internal to object. You can feed dragon, but not ask if hungry.
-
-  def hungry?
-    @stuff_in_belly <= 2
-  end
-
-  def poopy?
-    @stuff_in_intestine >= 8
-  end
-
-  def passage_of_time
-    if @stuff_in_belly > 0
-      @stuff_in_belly = @stuff_in_belly - 1
-      @stuff_in_intestine = @stuff_in_intestine + 1
-    else #out dragon is starving!
-      if @asleep
-        @asleep = false
-        puts 'He wakes up suddenly!'
-      end
-      puts @name + ' is starving! In desperation, he ate YOU!'
+    elsif (@years_old > 6 && @years_old < 9)
+      @years_old = @years_old + 1
+      @grows_fruit = true
+      @orange_count = @orange_count + 4 #grows 4 a year
+      @height = @height + 2
+      puts 'You tree grew 2 feet this year and 4 oranges! You now have ' + @orange_count.to_s + ' oranges and your tree is ' + @years_old.to_s + ' years old!'
+      puts 'Your tree is ' + @height.to_s + ' feet tall!'
+    else @years_old >= 9
+      tree_dies
       exit #Quits program
     end
-
-      if @stuff_in_intestine >= 10
-        @stuff_in_intestine = 0
-        puts 'Whoops ' + @name + ' had an accident...'
-      end
-
-      if hungry?
-        if @asleep
-          @asleep = false
-          puts 'He wakes up suddenly!'
-        end
-        puts @name + '\'s stomach grumbles...'
-      end
-
-      if poopy?
-        if @asleep
-          @asleep = false
-          puts 'He wakes up suddenly!'
-        end
-        puts @name + ' does the potty dance...'
-      end
-    end
-
   end
 
+  def tree_dies
+    if @years_old >= 9
+      @orange_count = 0
+      @grows_fruit= false
+      puts 'Your tree lived a good, fruitful life, but unfortunately shriveled up and died.'
+      exit #Quits program
+    else
+      puts 'Your tree is getting older!'
+    end
+  end
 
-pet = Dragon.new 'Norbert'
-pet.feed
-pet.toss
-pet.walk
-pet.put_to_bed
-pet.rock
-pet.put_to_bed
-pet.put_to_bed
-pet.put_to_bed
-pet.put_to_bed
+  def count_the_oranges
+    puts 'Your tree has ' + @orange_count.to_s + ' oranges!'
+    one_year_passes
+  end
+
+  def pick_orange
+    if @orange_count > 0
+    @orange_count = @orange_count - 1
+    puts 'That orange was freakin\' delicious! But now you\'re down to ' + @orange_count.to_s + ' oranges.'
+    else
+      puts 'Sorry buddy, but there aren\'t any oranges left this year.'
+    end
+  end
+
+end
+
+tree= Orange_tree.new
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.tree_dies
+tree.count_the_oranges
+tree.pick_orange
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
+tree.count_the_oranges
